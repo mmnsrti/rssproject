67a58a38f355af0584c9a3cb6e6f76831bb8804a
mmnsrti <mmnsrti@gmail.com>
2025-07-20 18:07:13 +0330

    `Added go-chi/chi and go-chi/cors dependencies and updated main.go to use them`

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/mmnsrti/rssproject
 
 go 1.24.5
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-chi/cors v1.2.2
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
-	
-
 
 func main() {
-	fmt.Print("Hello, World!\n")
 	godotenv.Load(".env")
-	portString := os.Getenv("PORT") 
+	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("Environment variable PORT is not set")
+	}
+	
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE","OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+	}))
+	log.Printf("Starting server on port %s", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error starting server:", err)
 	}
 
 	fmt.Println("Environment variable PORT is set to:", portString)
