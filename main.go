0b569eae863c526ac67e82f35837eeafa9d6d2e3
mmnsrti <mmnsrti@gmail.com>
2025-07-20 17:45:29 +0330

    `Added .env file to .gitignore and updated main.go to load environment variables using godotenv`

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
-package main 
+package main
 
-import ( "fmt")
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 	
 
 
 func main() {
 	fmt.Print("Hello, World!\n")
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT") 
+	if portString == "" {
+		log.Fatal("Environment variable PORT is not set")
+	}
+
+	fmt.Println("Environment variable PORT is set to:", portString)
 }
