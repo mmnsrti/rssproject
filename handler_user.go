665dc5f5a8c542ffc18535a9997b15400307fe8e
mmnsrti <mmnsrti@gmail.com>
2025-07-22 14:46:41 +0330

    `Refactor handlerCreateUser to handle POST requests and create users in the database`

--- handler_user.go
+++ handler_user.go
@@ -1,9 +1,27 @@
 package main
 
-import "net/http"
-func (apicfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	// This handler will create a new user
-	// Implementation will go here
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Create User endpoint not implemented yet"))
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/mmnsrti/rssproject/internal/database"
+)
+
+func (apicfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `name`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	apicfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+	})
+
+	respondWithJSON(w, 200, struct{}{})
 }
--- main.go
+++ main.go
@@ -54,14 +54,14 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerError)
-	v1Router.Get("/users", apicfg.handlerCreateUser)
+	v1Router.Post("/users", apicfg.handlerCreateUser)
 
+	
 	router.Mount("/v1", v1Router)
 	log.Printf("Starting server on port %s", portString)
-	err := srv.ListenAndServe()
+	log.Fatal(srv.ListenAndServe())
 	if err != nil {
 		log.Fatal("Error starting server:", err)
 	}
-
 	fmt.Println("Environment variable PORT is set to:", portString)
 }
